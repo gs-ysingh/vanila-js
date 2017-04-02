0f00575dd58c7d34b3ad166ba2d6018321cc99dd
Yogesh Singh <ysingh@gainsight.com>
2017-04-03 01:05:49 +0530

    More on promises

--- promise.js
+++ promise.js
@@ -1,3 +1,24 @@
 /**
  * Created by YSingh on 03/04/17.
  */
+
+//Promise example 1:
+
+var promiseToProject = new Promise(function (resolve, reject) {
+    var projectComplete = true;
+
+    if(projectComplete) {
+        resolve("Project is complete");
+    }
+    else {
+        reject("Project is not complete");
+    }
+});
+
+promiseToProject
+    .then(function (res) {
+        console.log(res);
+    })
+    .catch(function (res) {
+        console.log(res);
+    });

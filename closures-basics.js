184cde9848cf9a828f772fbe9dc3c2cc11606c98
Yogesh Singh <ysingh@gainsight.com>
2017-04-03 00:37:15 +0530

    More examples on closure

--- closures-basics.js
+++ closures-basics.js
@@ -16,11 +16,35 @@ f2() //a is not defined
 
 */
 
+//Example of lexical scope: inner function is not closures since it's not in global space. But, it creates scope
+//chain when it was defined and have access to m variable
+
+function outer(m) {
+    function inner() {
+        console.log(m++);
+    }
+    inner();
+}
+
+outer(10);
+
+
 //Closures: It's formed when function is already in global space or sent to global space
 
+//Simple example of closure
+
+var global = 10;
+
+var add = function () {
+    var val = 2;
+    return val + global;
+}
+console.dir(add);
+
+add() //It's closures, function is already in global space and have access to variable outside
+
+
 //closure 1: Returning a function in global space
-
-
 function outer(m) {
     return function inner() {
         console.log(m++);
@@ -56,3 +80,37 @@ var resObj = outer(10);
 resObj();
 resObj();
 resObj();
+
+
+
+//Problems that can be solved by closures;
+
+//Since three closures are sharing same variable, and closures does not preserves the updated value of variable
+//It's just creates a link
+//If value is updated later, closure will get updated value
+
+var a = [];
+for(var i = 0; i < 3; i++) {
+    a[i] = function () {
+        console.log(i);
+    }
+}
+
+a[0]();
+a[1]();
+a[2]();
+
+
+//Solving by creating 3 local variables
+var a = [];
+for(var i = 0; i < 3; i++) {
+    a[i] = function (i) {
+        return function () {
+            console.log(i);
+        }
+    }(i);
+}
+
+a[0]();
+a[1]();
+a[2]();

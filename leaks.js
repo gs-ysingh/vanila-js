d83eb6f44701c37b27f1ffe35ec39e49f65a8e26
Yogesh Singh <ysingh@gainsight.com>
2017-04-08 02:26:06 +0530

    leaks

---
+++ leaks.js
@@ -0,0 +1,39 @@
+/**
+ * Created by YSingh on 08/04/17.
+ */
+
+function outer() {
+    var temp1 = new Array(10000).join("x");
+    var temp2 = new Array(10000).join("y");
+
+    var res = {
+        temp1: temp1,
+        temp2: temp2
+    };
+
+    return function () {
+        console.log(res.temp1);
+        //Since we are accessing temp1, but still in memory it's holding reference to complete res object
+        //This is memory leaks, we need to handle such scenerio
+
+        //Step 1: We should never access large object's, variables inside closures
+    }
+}
+
+var obj = outer();
+obj();
+
+function outer() {
+    var temp1 = new Array(10000).join("x");
+    var temp2 = new Array(10000).join("y");
+
+    var res = {
+        temp1: temp1,
+        temp2: temp2
+    };
+
+    //One of possible way to re-write this function:
+    return function () {
+        console.log(new Array(10000).join("x"));
+    }
+}

08c76e075b3d8c9fac88d8fa2c9fb6d1e86215bb
Yogesh Singh <ysingh@gainsight.com>
2017-04-14 22:10:54 +0530

    jQuery like

---
+++ jQuery-like.js
@@ -0,0 +1,78 @@
+var $ = (function () {
+	var jQuery = function(selector) {
+		return new init(selector);
+	}	
+
+	//constructor
+	function init(selector) {
+		var elements;
+		
+		if(typeof selector == "string") {
+			elements = document.querySelectorAll(selector);
+		}
+		else if(selector.length) {
+			elements = selector;
+		}
+		else {
+			elements = [selector];
+		}
+
+		for (var i = elements.length - 1; i >= 0; i--) {
+			this[i] = elements[i];
+		}
+
+		this.length = elements.length;
+
+		return this;
+	}
+
+	//Adding method to constructor's prototype
+	
+	init.prototype = {
+		//$('.avatar').map(function(item, index) {return item.src || ""; } )
+		map: function(callback) {
+			var res = [];
+			for (var i = this.length - 1; i >= 0; i--) {
+				res.push(callback.call(this, this[i], i));
+			}
+			return res; //map need to return response
+		},
+		forEach: function(callback) {
+			this.map(callback);
+			return this; 
+			//forEach need to just call map but to implement chaining we can return this
+		},
+		text: function(text) {
+			return this.forEach(function(item) {
+				item.innerText = text;
+			});
+		},
+		html: function(html) {
+			return this.forEach(function(item) {
+				item.innerHTML = html;
+			});
+		},
+		addClass: function(classes) {
+			var className = "";
+			className += classes;
+
+			return this.forEach(function(ele) {
+				ele.className += className;
+			});
+		},
+		attr: function(attr, val) {
+			return this.forEach(function(ele) {
+				ele.setAttribute(attr, val);
+			});
+		},
+		on: (function() {
+			return function(evt, handler) {
+				return this.forEach(function(ele) {
+					ele.addEventListener(evt, handler, false);
+				});
+			}
+		})()
+	} 
+
+	return jQuery;	
+})();
